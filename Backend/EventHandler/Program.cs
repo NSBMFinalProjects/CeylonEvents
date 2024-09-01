1e5e6593090fb75c997a7ee688e5bcde74bd11af
KDDilshan <kdprojects847@gmail.com>
2024-09-02 01:03:03 +0530

    set database conntion

--- Backend/EventHandler/Program.cs
+++ Backend/EventHandler/Program.cs
@@ -1,6 +1,17 @@
+using EventHandler.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<EventDbContext>(options => 
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"));
+});
+
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
