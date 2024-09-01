2832a8f8f74eff3bdbd04a03309e8c6f923c313b
KDDilshan <kdprojects847@gmail.com>
2024-09-01 20:24:15 +0530

    aded dbconext class for databse connection

---
+++ Backend/EventHandler/Data/EventDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EventHandler.Data
+{
+    public class EventDbContext : DbContext
+    {
+        public EventDbContext(DbContextOptions<EventDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Event> Events { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Session> Sessions { get; set; }
+
+        public DbSet<Ticket> Ticket { get; set; }
+
+        public DbSet<Notification> Notifications { get; set; }
+
+        public DbSet<EventAttendance> EventAttendances { get; set; }
+
+        public DbSet<UserRole> UserRoles { get; set; }  
+    }
+}
