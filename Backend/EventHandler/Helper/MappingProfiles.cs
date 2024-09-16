eb8192ddc1b6d168d50b45665bf9a42c16f47a97
KDDilshan <kdprojects847@gmail.com>
2024-09-16 12:03:23 +0530

    modified mapping username feature

--- Backend/EventHandler/Helper/MappingProfiles.cs
+++ Backend/EventHandler/Helper/MappingProfiles.cs
@@ -1,6 +1,15 @@
-﻿namespace EventHandler.Helper
+﻿using AutoMapper;
+using EventHandler.Dto;
+using EventHandler.Models.Entities;
+
+namespace RestApiwithDB.Helper
 {
-    public class MappingProfiles
+    public class MappingProfiles : Profile
     {
+        public MappingProfiles()
+        {
+            CreateMap<RegisterDto, AppUser>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Email));
+        }
     }
 }
