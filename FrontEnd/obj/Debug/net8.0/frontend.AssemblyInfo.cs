6cedaa8c0e1d32e1fc3fd23cc2cbd12ee0e7a11e
Navindu-Ashen <navindu158@gmail.com>
2024-09-16 23:35:59 +0530

    feat(register-page): implements register page UI

--- FrontEnd/obj/Debug/net8.0/frontend.AssemblyInfo.cs
+++ FrontEnd/obj/Debug/net8.0/frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83fea3272d6063fc8bb88dd0de0387794d2bf6d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5be9e971c499f75882d0e948056413a39c32f06")]
 [assembly: System.Reflection.AssemblyProductAttribute("frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
