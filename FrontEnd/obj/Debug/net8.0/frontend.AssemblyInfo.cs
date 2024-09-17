ded5e32b0dff03a0f0331dbbfff16686a835d0d9
Navindu-Ashen <navindu158@gmail.com>
2024-09-18 01:30:42 +0530

    feat(profile-page): implements profile page

--- FrontEnd/obj/Debug/net8.0/frontend.AssemblyInfo.cs
+++ FrontEnd/obj/Debug/net8.0/frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7033e10913053fc382101697d280ec96eef754d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5083217d240b24b3d5c052a103a4b70e76dcef2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
