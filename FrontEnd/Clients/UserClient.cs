77307de53eb88809b0b1633141cba7861d4e7034
Navindu-Ashen <navindu158@gmail.com>
2024-09-24 01:01:07 +0530

    fix(login): fixes user login and authorized page access.

--- FrontEnd/Clients/UserClient.cs
+++ FrontEnd/Clients/UserClient.cs
@@ -1,21 +1,47 @@
 using System;
+using System.Net.Http.Headers;
 using frontend.Models;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 
 namespace frontend.Clients
 {
-    public class UserClient(HttpClient httpClient)
+    public class UserClient(HttpClient httpClient, ProtectedLocalStorage localStorage)
     {
+        public async Task SetAuthorizedHeader()
+        {
+            var token = (await localStorage.GetAsync<string>("authToken")).Value;
+            if (token != null)
+            {
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+        }
+
+        public async Task<bool> IsUserLogged()
+        {
+            var token = (await localStorage.GetAsync<string>("authToken")).Value;
+            if (token != null)
+            {
+                return true;
+            }else{
+                return false;
+            }
+        }
         public async Task RegisterUserAsync(User user) => await httpClient.PostAsJsonAsync("api/user/register", user);
 
         public async Task<LoginResponce> LoginUserAsync(LoginRequest request)
         {
-            var response = await httpClient.PostAsJsonAsync("api/User/login", request); // Corrected endpoint if needed
+            var response = await httpClient.PostAsJsonAsync("api/User/login", request);
 
             var loginResponse = await response.Content.ReadFromJsonAsync<LoginResponce>();
 
             return loginResponse ?? throw new InvalidOperationException("Login response is null.");
         }
 
+        public async Task<User> GetUserInfo()
+        {
+            await SetAuthorizedHeader();
+            return await httpClient.GetFromJsonAsync<User>("api/user") ?? throw new Exception("Could not find the user");
+        }
     }
 
 }
