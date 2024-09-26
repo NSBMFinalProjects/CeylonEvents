cd374d6c6552180cfdcd22a63cc73d23dacc6b6b
KDDilshan <kdprojects847@gmail.com>
2024-09-26 23:53:38 +0530

    new dto

---
+++ Backend/EventHandler/Dto/CreateEventDto.cs
@@ -0,0 +1,16 @@
+﻿public class EventRequestModel
+{
+    public string Name { get; set; }
+    public DateTime Date { get; set; }
+    public string Location { get; set; }
+    public int CategoryId { get; set; }  // CategoryId to relate to the category
+    public string OrganizerId { get; set; } // OrganizerId to relate to the organizer
+    public List<TicketRequestModel> Tickets { get; set; }
+}
+
+public class TicketRequestModel
+{
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+    public string TicketType { get; set; }
+}
