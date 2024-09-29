7d68fd9c206b2ccc9290dd191e910ee415b98de4
KDDilshan <kdprojects847@gmail.com>
2024-09-29 23:45:53 +0530

    Added new event category use sending dto

---
+++ Backend/EventHandler/Dto/EventCategoryDto.cs
@@ -0,0 +1,14 @@
+﻿namespace EventHandler.Dto
+{
+    public class EventCategoryDto
+    {
+        public int EventId { get; set; }
+        public string EventName { get; set; }
+        public string EventDate { get; set; }
+        public string EventTime { get; set; }
+        public string EventLocation { get; set; }
+        public string EventTicketPrice { get; set; }
+        public string EventDescription { get; set; }
+        public string EventImage { get; set; }
+    }
+}
