04d10a0748f2e9c794729fecffb4dcb5d47cfb11
KDDilshan <kdprojects847@gmail.com>
2024-09-29 23:38:01 +0530

    added new feilds and updated endpoints

--- Backend/EventHandler/Controllers/EventController.cs
+++ Backend/EventHandler/Controllers/EventController.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Security.Claims;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using EventHandler.Data;
 using EventHandler.Dto;
 using EventHandler.Helper;
+using EventHandler.Models.Entities;
 using EventHandler.Services.EventService;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace EventHandler.Controllers
 {
@@ -35,17 +40,19 @@ namespace EventHandler.Controllers
         public async Task<IActionResult> GetEventById(int id)
         {
             var eventEntity = await _eventService.GetEventDetails(id);
-            if (eventEntity == null)
+
+            if (eventEntity == null || eventEntity.EventType != "Approved")
             {
                 return NotFound();
             }
+
             var imageUrl = $"{Request.Scheme}://{Request.Host}/Uploads/{eventEntity.Image}";
             var eventDto = new EventDto
             {
 
                 EventId = eventEntity.Id,
                 EventName = eventEntity.EventName,
-                EventDate = eventEntity.StartDate.ToShortDateString(),
+                EventDate = eventEntity.StartDate.ToString("yyyy-MM-dd"),
                 EventTime = eventEntity.StartDate.ToShortTimeString(),
                 EventLocation = eventEntity.Location,
                 EventTicketPrice = eventEntity.tickets?.FirstOrDefault()?.Price.ToString("c") ?? "no tickets",
@@ -57,24 +64,26 @@ namespace EventHandler.Controllers
             return Ok(eventDto);
         }
 
-        [HttpGet]
+        [HttpGet("all")]
         public async Task<IActionResult> GetAllEvents()
         {
             var eventEntites = await _eventService.GetAllEventsAsync();
 
-            if (eventEntites == null || !eventEntites.Any())
+            var approvedEvents = eventEntites
+                                .Where(eventEntity => eventEntity.EventType == "Approved") 
+                                .ToList();
+
+            if (eventEntites == null || !approvedEvents.Any())
             {
-                return NotFound(" no events Found");
+                return NotFound(" No approved events found");
             }
 
-            
-
-            var eventDtos = eventEntites.Select(eventEntity => new EventDto
+            var eventDtos = approvedEvents.Select(eventEntity => new EventDto
             {
 
                 EventId = eventEntity.Id,
                 EventName = eventEntity.EventName,
-                EventDate = eventEntity.StartDate.ToShortDateString(),
+                EventDate = eventEntity.StartDate.ToString("yyyy-MM-dd"),
                 EventTime = eventEntity.StartDate.ToShortTimeString(),
                 EventLocation = eventEntity.Location,
                 EventTicketPrice = eventEntity.tickets?.FirstOrDefault()?.Price.ToString("c") ?? "no tickets",
@@ -89,95 +98,36 @@ namespace EventHandler.Controllers
             return Ok(eventDtos);
         }
 
-        [HttpPost]
-        [Authorize(Roles = "Organiser")]
-        [Consumes("multipart/form-data")]
-        public async Task<IActionResult> CreateEvent([FromForm] EventWithTicketsDto eventDto, [FromForm] string tickets, [FromForm] IFormFile imageFile)
+        [HttpGet("category/{categoryId}")]
+        public async Task<IActionResult> GetEventByCategory([FromRoute] int categoryId)
         {
-            //cheak for validation
-            if (eventDto == null|| imageFile==null)
+            var category = await _context.Categorys
+                .Include(c => c.Events) // One to many
+                .FirstOrDefaultAsync(c => c.Id == categoryId);
+
+            if (category == null)
             {
-                return BadRequest(new { Message = "Invalid user data or file" });
+                return NotFound($"Category with ID {categoryId} not found");
             }
 
-            if(string.IsNullOrWhiteSpace(eventDto.EventName))
+            var eventDtos = category.Events.Select(e => new EventCategoryDto
             {
-                return BadRequest("Event name is required.");
-            }
+                EventId = e.Id,
+                EventName = e.EventName,
+                EventDate = e.StartDate.ToString("yyyy-MM-dd"), 
+                EventTime = e.StartDate.ToShortTimeString(),             
+                EventLocation = e.Location,
+                EventTicketPrice = e.tickets?.FirstOrDefault()?.Price.ToString("c") ?? "no tickets", 
+                EventDescription = e.Description,
+                EventImage = e.Image
+            }).ToList();
 
-            if (eventDto.StartDate == DateTime.MinValue || eventDto.EndDate == DateTime.MinValue)
-            {
-                return BadRequest("Start and end dates are required.");
-            }
+            return Ok(eventDtos);
 
-            if (eventDto.EndDate < eventDto.StartDate)
-            {
-                return BadRequest("End date cannot be before the start date.");
-            }
-
-            if (eventDto.Tickets == null || !eventDto.Tickets.Any())
-            {
-                return BadRequest("At least one ticket must be provided.");
-            }
-
-            foreach (var ticket in eventDto.Tickets)
-            {
-                if (string.IsNullOrWhiteSpace(ticket.TicketName))
-                {
-                    return BadRequest("Ticket name is required.");
-                }
-
-                if (ticket.Quantity <= 0)
-                {
-                    return BadRequest("Ticket quantity must be greater than zero.");
-                }
-
-                if (ticket.Price < 0)
-                {
-                    return BadRequest("Ticket price cannot be negative.");
-                }
-            }
-
-            //handle File Upload
-            var result = await _uploadHandler.UploadAsync(imageFile);
-
-            if (result.StartsWith("Extension is not valid") || result.StartsWith("Maximum size"))
-            {
-                return BadRequest(new { Message = result });
-            }
-
-            //get current user and assign to userid
-            var loggedInUserId = User.FindFirstValue(ClaimTypes.NameIdentifier); 
-
-            if (eventDto.OrganizerId != loggedInUserId)
-            {
-                return StatusCode(403, new { message = "You are not authorized to create this event." });
-            }
-
-            var eventEntity = new Event
-            {
-                OrganizerId = loggedInUserId,
-                EventName = eventDto.EventName,
-                Description = eventDto.Description,
-                StartDate = eventDto.StartDate,
-                EndDate = eventDto.EndDate,
-                Location = eventDto.Location,
-                CategoryId = eventDto.CategoryId,
-                Image= result,
-                tickets = eventDto.Tickets.Select(t => new Ticket
-                {
-                    TicketName = t.TicketName,
-                    Quantity = t.Quantity,
-                    Price = t.Price,
-                }).ToList()
-            };
-
-           
-            _context.Events.Add(eventEntity);
-            await _context.SaveChangesAsync();
-
-            return Ok(new { message = "Event created successfully", eventId = eventEntity.Id });
         }
+
+        
+
 
 
 
