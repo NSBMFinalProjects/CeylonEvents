ab4dbf16806f013739f9726e0a9ce56a186fdaea
KDDilshan <kdprojects847@gmail.com>
2024-10-06 13:08:14 +0530

    added new role assign service

---
+++ Backend/EventHandler/Services/RoleService/RoleService.cs
@@ -0,0 +1,32 @@
+﻿using EventHandler.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace EventHandler.Services.RoleService
+{
+    public class RoleService
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RoleService(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IdentityResult> AssignOrganizerRole(AppUser user)
+        {
+            
+            // Check if the user already has the "Organiser" role
+            var isInRole = await _userManager.IsInRoleAsync(user, "Organiser");
+            if (isInRole)
+            {
+                return IdentityResult.Failed(new IdentityError { Description = "User is already in the Organiser role." });
+            }
+
+            // Assign the "Organiser" role to the user
+            return await _userManager.AddToRoleAsync(user, "Organiser");
+            
+        }
+    
+    }
+
+}
