bd7228b7bb46469ccd42f5e8a69597e26b7561a5
KDDilshan <kdprojects847@gmail.com>
2024-10-06 13:09:11 +0530

    added new dto for sending request detils for user

---
+++ Backend/EventHandler/Dto/RequestDto.cs
@@ -0,0 +1,13 @@
+﻿namespace EventHandler.Dto
+{
+    public class RequestDto
+    {
+        public int Id { get; set; }
+
+        public string ReqDetails { get; set; }
+
+        public string UserId { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
